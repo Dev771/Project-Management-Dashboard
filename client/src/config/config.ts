65710486d583824d9349c7afef636012e709197c
Dev Garg <devgarg771@gmail.com>
2025-05-10 14:53:40 +0530

    Changes

--- client/src/config/config.ts
+++ client/src/config/config.ts
@@ -1,4 +1,4 @@
 export const config = {
-    "apiUrl": "https://project-management-dashboard-yj77.onrender.com",
+    "apiUrl": "https://project-management-dashboard-yj77.onrender.com/api",
     "socketUrl": "wss://project-management-dashboard-yj77.onrender.com"
 }
